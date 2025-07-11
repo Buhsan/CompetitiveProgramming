62ac33a705975303e46b8d02912f2b32b4022af1
Buhsan <buhsan@buhsan.dev>
2025-07-11 17:00:03 +0200

    Update for problem D

--- AceptaElReto/Maraton24en23/D/file.cpp
+++ AceptaElReto/Maraton24en23/D/file.cpp
@@ -1,43 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string check(string sample, vector<string> pull) {
-  vector<string> result;
-  int n = pull.size();
-  string s = sample;
-  sort(s.begin(), s.end());
-  for (int i = 0; i < n; i++) {
-    string aux = pull[i];
-    string aux1 = pull[i];
-    sort(aux1.begin(), aux1.end());
-    if ((sample[0] == aux[0] &&
-         sample[sample.size() - 1] == aux[aux.size() - 1]) &&
-        (s == aux1))
-      result.push_back(pull[i]);
-  }
-  if (result.size() == 1)
-    return result[0];
-  return "";
+string obtener_clave(const string& palabra) {
+    if (palabra.size() <= 3) return palabra;
+    string clave;
+    clave += palabra.front();
+    string medio = palabra.substr(1, palabra.size() - 2);
+    sort(medio.begin(), medio.end());
+    clave += medio;
+    clave += palabra.back();
+    return clave;
 }
 
 int main() {
-  int n;
-  cin >> n;
-  for (int i = 0; i < n; i++) {
-    unordered_map<int, vector<string>> dic;
-    string s;
-    while (cin >> s && s != ".") {
-      dic[s.size()].push_back(s);
-    }
+    int t;
+    cin >> t;
+    cin.ignore();
 
-    while (cin >> s && s != ".") {
-      string aux = check(s, dic[s.size()]);
-      if (aux.empty()) {
-        cout << s << " ";
-      } else
-        cout << aux << " ";
+    while (t--) {
+        unordered_map<string, string> clave_a_palabra;
+        unordered_map<string, bool> es_ambigua;
+        unordered_map<string, bool> es_exacta;
+
+        string linea;
+        getline(cin, linea);
+        stringstream ss1(linea);
+        string palabra;
+        while (ss1 >> palabra && palabra != ".") {
+            string clave = obtener_clave(palabra);
+            es_exacta[palabra] = true;
+            if (!clave_a_palabra.count(clave)) {
+                clave_a_palabra[clave] = palabra;
+                es_ambigua[clave] = false;
+            } else {
+                es_ambigua[clave] = true;
+            }
+        }
+
+        getline(cin, linea);
+        stringstream ss2(linea);
+        bool primero = true;
+        while (ss2 >> palabra && palabra != ".") {
+            if (!primero) cout << " ";
+            primero = false;
+
+            if (es_exacta.count(palabra)) {
+                cout << palabra;
+            } else {
+                string clave = obtener_clave(palabra);
+                if (!es_ambigua[clave] && clave_a_palabra.count(clave)) {
+                    cout << clave_a_palabra[clave];
+                } else {
+                    cout << palabra;
+                }
+            }
+        }
+        cout << " ." << '\n';
     }
-    cout << "." << endl;
-  }
-  return 0;
+    return 0;
 }
